02c1ce555e28d58fc1489fd5f325b8401f0db0eb
Simon Müller <simon.f.mueller@gmail.com>
2016-02-17 11:02:35 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -7,7 +7,7 @@
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-14 \
 	gluon-alfred \
-	gluon-announced \
+	gluon-respondd \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-contact-info \
